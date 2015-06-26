370f0b6cb9175d13a188710ab11e7a93df91f672
mustika17mumu <60200112101@uin-alauddin.ac.id>
2015-06-26 22:49:20 +0800

    versi 1

---
+++ src/naimah/naimah.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package naimah;
+
+/**
+ * @author acer
+ *
+ */
+public class naimah {
+
+	/**
+	 * @param args
+	 */
+	
+	static String pengetahuan1 = "Print";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String praktikum1 = "BIODATA";
+		System.out.println(praktikum1);
+		biodata();
+		pembagian();
+
+	}
+	
+	public static void biodata()
+	{
+		System.out.println("Riskal");
+		System.out.println("60200112111");
+		System.out.println("KelasC");
+	}
+	
+	public static void pembagian()
+	{
+		float a = 10/2;
+		int b=6/8;
+		System.out.println("PEMBAGIAN");
+		System.out.println("Hasilnya Adalah:"+" "+a);
+		System.out.println("Hasilnya Adalah:"+" "+b);
+	}
+
+}
